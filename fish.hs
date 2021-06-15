7633136186ae449b98e34a9e05579c3c610d4628
Sam Alws <sam@samalws.com>
2021-06-14 22:42:26 -0400

    initial

---
+++ fish.hs
@@ -0,0 +1,60 @@
+import qualified Data.Set as S
+import Data.Maybe
+
+nonDup :: (Ord a) => [a] -> Bool
+nonDup l = length l == length (S.fromList l)
+
+-- ord derivations are so they can be put into sets
+data Card = Card { cardNum :: Int, cardSuit :: Int }              deriving (Show, Eq, Ord)
+data Plr  = Plr  { plrNum :: Int }                                deriving (Show, Eq, Ord)
+data Team = Team { teamNum :: Int }                               deriving (Show, Eq, Ord)
+data Move = Move { moveCard :: Card, mover :: Plr, movee :: Plr } deriving (Show, Eq, Ord)
+data GameState = GameState { gamePlrs :: [(Plr, Team)], gameScores :: [(Team, Int)], gameHands :: [(Plr, [Card])], gameDran :: Plr } deriving (Eq, Show)
+
+fullDeck :: [Card]
+fullDeck = [Card num suit | num <- [1..6], suit <- [1..9]]
+
+fullDeckSet :: S.Set Card
+fullDeckSet = S.fromAscList fullDeck
+
+validCard :: Card -> Bool
+validCard = flip S.member fullDeckSet
+
+validGameState :: GameState -> Bool
+validGameState s = matchingPlrs && matchingTeams && validHands && validDran && nonDupPlrs && nonDupHandPlrs && nonDupScores && nonDupHands where
+
+  plrList1  = fst <$> gamePlrs   s
+  plrList2  = fst <$> gameHands  s
+  teamList1 = snd <$> gamePlrs   s
+  teamList2 = fst <$> gameScores s
+  cardList  = concat $ snd <$> gameHands s
+
+  matchingPlrs  = S.fromList plrList1  == S.fromList plrList2
+  matchingTeams = S.fromList teamList1 == S.fromList teamList2
+
+  validHands = all validCard cardList
+  validDran  = gameDran s `elem` plrList1
+
+  nonDupPlrs     = nonDup plrList1
+  nonDupHandPlrs = nonDup plrList2
+  nonDupScores   = nonDup teamList2
+  nonDupHands    = nonDup cardList
+
+plrHand :: Plr -> GameState -> Maybe [Card]
+plrHand p = lookup p . gameHands
+
+plrTeam :: Plr -> GameState -> Maybe Team
+plrTeam p = lookup p . gamePlrs
+
+validMove :: GameState -> Move -> Bool
+validMove g m = goodCardSuit && goodCard && validCheckCard && moveeExists && goodMovee && moveeNonempty && goodMover where
+  moverHand = fromMaybe [] $ plrHand (mover m) g
+  goodCardSuit = cardSuit (moveCard m) `elem` fmap cardSuit moverHand
+  goodCard = not $ elem (moveCard m) moverHand
+  validCheckCard = validCard $ moveCard m
+  moveeExists = elem (movee m) $ fst <$> gamePlrs g
+  goodMovee = plrTeam (movee m) g /= plrTeam (mover m) g
+  moveeNonempty = plrHand (movee m) g /= Just []
+  goodMover = mover m == gameDran g
+
+main = return ()
